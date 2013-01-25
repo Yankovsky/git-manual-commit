46d9a521c299f21ff9a061a1acdbbc3ea5297512
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-01-25 06:01:35 +0400

    manual commit now respects its ancestors

--- git-manual-commit.sh
+++ git-manual-commit.sh
@@ -1,11 +1,14 @@
+#prepare git working directory
 mkdir git-manual-commit
 cd git-manual-commit
 git init
+
+#make manual commit
 mkdir dir1
 echo Contents of foo! > dir1/foo.txt
 cp ../bar.png dir1/
 git add .
-git write-tree | xargs git commit-tree -m "manual commit creating" | xargs git update-ref HEAD
+git write-tree | xargs git commit-tree -p HEAD -m "manual commit creating" | xargs git update-ref HEAD
 
 #add readme and this script, then push all changes to github 
 git remote add origin git@github.com:Yankovsky/git-manual-commit.git
