957bceef19abeb58d25badcc0d18b03ed75cecf9
Yankovsky <YankovskyAndrey@gmail.com>
2013-02-02 04:18:34 +0400

    manual commit second attempt

--- git-manual-commit.sh
+++ git-manual-commit.sh
@@ -1,19 +1,12 @@
-#prepare git working directory
-mkdir git-manual-commit
-cd git-manual-commit
-git init
-
-#make manual commit
-mkdir dir1
-echo Contents of foo! > dir1/foo.txt
-cp ../bar.png dir1/
-git add .
-git write-tree | xargs git commit-tree -p HEAD -m "manual commit creating" | xargs git update-ref HEAD
-
-#add readme and this script, then push all changes to github 
-git remote add origin git@github.com:Yankovsky/git-manual-commit.git
-cp ../git-manual-commit.sh .
-echo see solution in git-manual-commit.sh script > README.md
-git add .
-git commit -am "adding git-manual-commit.sh script"
-git push -u origin master
+mkdir dir2
+echo Contents of foo! > dir2/foo.txt
+cp bar.png dir2/
+git hash-object -w dir2/foo.txt | xargs -I {} git update-index --add --cacheinfo 100644 {} dir2/foo.txt
+git hash-object -w dir2/bar.png | xargs -I {} git update-index --add --cacheinfo 100644 {} dir2/bar.png
+git update-index git-manual-commit.sh
+tree=`git write-tree`
+echo manual commit second attempt > changelog
+head=`git rev-parse HEAD`
+git commit-tree $tree -p $head < changelog | xargs git update-ref HEAD
+rm changelog
+git push origin master
